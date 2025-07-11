50adf95cfb81193c77273f4e2201d2069a58c2e7
Benoit Varillon <benoit.varillon@gmail.com>
2025-07-11 12:16:03 +0200

    make parser

---
+++ jlox/src/main/java/fr/bvarillon/lox/TokenType.java
@@ -0,0 +1,21 @@
+package fr.bvarillon.lox;
+
+public enum TokenType {
+    // Single character
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE, COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+    // One or two charecters 
+    BANG, BANG_EQUAL,
+    EQUAl, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL,
+    LOWER, LOWER_EQUAL, 
+
+
+    // Literals
+    STRING, NUMBER, IDENTIFIER,
+
+    // Keywords
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR, PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    EOF
+}
